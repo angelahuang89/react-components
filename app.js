dccc32ba63c81cb692ddec71814fb56ea003598e
Angela Huang <angelahuang89@gmail.com>
2017-10-25 12:13:27 -0700

    Render react components

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <ul>
+      <Cereal/>
+      <Milk/>
+    </ul>
+  </div>
+);
+
+var Cereal = () => (
+  <li>
+    cereal
+  </li>
+);
+
+var Milk = () => (
+  <li>
+    milk
+  </li>
+)
+
+var GroceryListItem = (props) => (
+  <li>
+    {props}
+  </li>
+)
+
+ReactDOM.render(<GroceryList/>, document.getElementById("app"));
